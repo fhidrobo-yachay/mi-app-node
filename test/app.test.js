d1b13e0216ad16814149c6bb1b790e686b82940e
Francisco Hidrobo <fhidrobo@yachaytech.edu.ec>
2026-05-18 09:49:52 -0500

    Despliegue en Render reviwed

--- test/app.test.js
+++ test/app.test.js
@@ -5,12 +5,12 @@ test('GET / debe devolver un mensaje', async () => {
   const response = await request(app).get('/');
 
   expect(response.statusCode).toBe(200);
-  expect(response.body.message).toBe('Hola desde Node.js y GitHub Actions');
+  expect(response.body.message).toBe('Hola desde Node.js con GitHub Actions en Render');
 });
 
 test('GET /health debe devolver status ok', async () => {
   const response = await request(app).get('/health');
 
   expect(response.statusCode).toBe(200);
-  expect(response.body.status).toBe('ok');
+  expect(response.body.status).toBe('ok. Todo está funcionando correctamente');
 });
